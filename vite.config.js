ce1f19447a1331bab39f9c548e89e3ad26f480f4
naitikprime07 <naitikprime07@gmail.com>
2026-09-21 18:18:33 +0530

    add privacy and terms condition file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
+
+const r = (p) => fileURLToPath(new URL(p, import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +11,15 @@ export default defineConfig({
     transformer: 'postcss'
   },
   build: {
-    cssMinify: false
+    cssMinify: false,
+    rollupOptions: {
+      input: {
+        main: r('./index.html'),
+        game: r('./game.html'),
+        classify: r('./classify.html'),
+        gamedet: r('./gamedet.html'),
+        search: r('./search.html')
+      }
+    }
   }
 })
